36ee6edde1e2069861dcfd420cead4c2e98d2311
Rodrigo Lima <rodrigodlima@gmail.com>
2015-01-29 16:30:19 -0200

    Removendo zimbra do branch master

--- zimbra/limpa_fila.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#Script para remover mensagens de MAILER-DAEMON da fila
-
-for i in $(/opt/zimbra/postfix/sbin/mailq |grep MAILER-DAEMON|awk -F" " '{print $1}' |awk -F"*" '{print $1}'); do /opt/zimbra/postfix/sbin/postsuper -d $i; done
--- zimbra/mailq.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#Script para contar as mensagens na fila do Zimbra. Esse script é utilizado pelo Zabbix para monitorar a fila do servidor.
-
-/opt/zimbra/postfix/sbin/mailq |tail -n1 | awk -F" " '{print $5}'
